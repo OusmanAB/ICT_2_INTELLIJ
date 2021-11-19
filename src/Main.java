d725eaaf180a9277ad31122f2c586d972ae82178
Krawist <arthurngobengmail.com>
2021-11-19 10:23:00 +0100

    création des classes UE et matière

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        int variable = 10;
-
-        if(variable > 20){
-            System.out.println("variable supérieure \n\n ");
-        }else{
-            System.out.println("Variable inférieure \n \n");
-        }
+        System.out.printf("Hello world");
     }
 }
