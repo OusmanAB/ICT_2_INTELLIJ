ceac83b91e9213d4b5986266c7c77358322ddf73
Arthur Krawist <arthurngoben@gmail.com>
2021-11-19 10:08:18 +0100

    initialisation du dépot

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int variable = 10;
+
+        if(variable > 20){
+            System.out.println("variable supérieure \n\n ");
+        }else{
+            System.out.println("Variable inférieure \n \n");
+        }
+    }
+}
